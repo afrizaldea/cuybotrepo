1e6908d8740ace7ec631ee789941e6277fa23907
afrizaldea <>
2021-10-04 06:06:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import discord
+import os
+import requests
+import json
+from liveserver import liveserver
+import locale
+locale.setlocale(locale.LC_ALL, '')
+
+
+client = discord.Client()
+
+def get_covid_data(args, params, child):
+  response = requests.get('https://data.covid19.go.id/public/api/update.json')
+  json_data = json.loads(response.text)
+  data = ''
+  if args != '' and params != '' and child != '':
+   data = json_data[args][params][child]
+  elif args != '' and params != '' and child == '':
+   data = json_data[args][params]
+  else:
+    data = json_data[args]
+  return(data)
+
+@client.event
+async def on_ready():
+  print('logged in as {0.user} '.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  req_msg= message.content
+  bot_response = message.channel.send
+
+  if req_msg.startswith('cuy/status'):
+    await message.channel.send(':partying_face: CuyBot Masih Aktif! :partying_face:')
+
+  if req_msg.startswith('cuy/hi'):
+    await message.channel.send(':partying_face: Oy cuy! :partying_face: \n\nperkenalkan cuy gw bot buatannya dea dan tim :yum:\ngw siap bantu ngasih info info sesuatu yang lu butuhin')
+
+  if req_msg.startswith('cuy/covid'):
+    odp = get_covid_data('data','jumlah_odp','')
+    total_spesimen_negatif = get_covid_data('data', 'total_spesimen_negatif', '')
+    total_positif = get_covid_data('update', 'penambahan', 'jumlah_positif')
+    total_dirawat =   get_covid_data('update', 'penambahan','jumlah_dirawat')
+    total_sembuh = get_covid_data('update', 'penambahan','jumlah_sembuh')
+    total_meninggal = get_covid_data('update', 'penambahan','jumlah_meninggal')
+    update_per = get_covid_data('update', 'penambahan', 'created')
+    data_from = 'https://data.covid19.go.id'
+
+    await bot_response(':flag_mc: ***DATA COVID*** :flag_mc:\nUPDATE PER: ' + update_per + ' :date:\n' + ':warning: Data Resmi Dari: ' + data_from + ' :warning:\n\n' + 'Total ODP saat ini: **' + f'{odp:,}' + '** orang :thermometer_face:' + "\n" + 'Total spesimen negatif: **' + f'{total_spesimen_negatif:,}' + '** orang :thinking:' + '\n' + 'total positif: **' + f'{total_positif:,}' + '** orang :persevere:' + '\n' + 'Total sembuh: **' + f'{total_sembuh:,}' + '** orang :hugging:' + '\n' + 'Total meninggal: **' + str(total_meninggal) + '** orang :cry:' + '\n\n' + '---TERIMAKASIH CUYBOT--- :laughing:')
+
+liveserver()
+client.run(os.getenv('TOKEN'))
