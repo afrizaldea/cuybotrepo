8df60e841b19e500f060f78c10e7c5080d6bf542
dea <dea.aprizal@icloud.com>
2021-10-05 15:22:39 +0700

    fix tab

--- botReply.py
+++ botReply.py
@@ -22,22 +22,17 @@ class botReply:
             # lirik_lagu = lirik_detail(daftar_lagu)
             await bot_response(daftar_lagu)
 
-            if req_msg.startswith('cuy/covid'):
-                odp = api.get_covid_data('data', 'jumlah_odp', '')
-                total_spesimen_negatif = api.get_covid_data(
-                    'data', 'total_spesimen_negatif', '')
-                total_positif = api.get_covid_data(
-                    'update', 'penambahan', 'jumlah_positif')
-                total_sembuh = api.get_covid_data(
-                    'update', 'penambahan', 'jumlah_sembuh')
-                total_meninggal = api.get_covid_data(
-                    'update', 'penambahan', 'jumlah_meninggal')
-                update_per = api.get_covid_data(
-                    'update', 'penambahan', 'created')
+        if req_msg.startswith('cuy/covid'):
+            odp = api.get_covid_data('data', 'jumlah_odp', '')
+            total_spesimen_negatif = api.get_covid_data('data', 'total_spesimen_negatif', '')
+            total_positif = api.get_covid_data('update', 'penambahan', 'jumlah_positif')
+            total_sembuh = api.get_covid_data('update', 'penambahan', 'jumlah_sembuh')
+            total_meninggal = api.get_covid_data('update', 'penambahan', 'jumlah_meninggal')
+            update_per = api.get_covid_data('update', 'penambahan', 'created')
 
             await bot_response(':flag_mc: ***DATA COVID*** :flag_mc:\nUPDATE PER: ' + update_per + ' :date:\n' + ':warning: Data Resmi Dari: ' + c.data_covid_from + ' :warning:\n\n' + 'Total ODP saat ini: **' + f'{odp:,}' + '** orang :thermometer_face:' + "\n" + 'Total spesimen negatif: **' + f'{total_spesimen_negatif:,}' + '** orang :thinking:' + '\n' + 'total positif: **' + f'{total_positif:,}' + '** orang :persevere:' + '\n' + 'Total sembuh: **' + f'{total_sembuh:,}' + '** orang :hugging:' + '\n' + 'Total meninggal: **' + str(total_meninggal) + '** orang :cry:' + '\n\n' + '---TERIMAKASIH CUYBOT--- :laughing:')
 
-            if req_msg.startswith('cuy/cuaca'):
-                print('request => ' + req_msg)
-                data = api.get_cuaca('hari ini')
-                await bot_response(data)
+        if req_msg.startswith('cuy/cuaca'):
+            print('request => ' + req_msg)
+            data = api.get_cuaca('hari ini')
+            await bot_response(data)
