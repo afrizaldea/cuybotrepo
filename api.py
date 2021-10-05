15290bcd492e151e8bf101a0b7ed82269a16d682
Tedi Suryadi <32906420+Teday@users.noreply.github.com>
2021-10-05 15:09:03 +0000

    add quotes cuy

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import random
 
 def get_cuaca(param):
   return 'get ramalan cuaca' + param
@@ -33,3 +34,14 @@ def get_covid_data(args, params, child):
   else:
     data = json_data[args]
   return(data)
+
+def get_quotes():
+  response = requests.get('https://backend-ihsandevs.herokuapp.com/api/quotes%20keren/')
+  json_data = json.loads(response.text)
+  random_length = random.randint(0,len(json_data['result']) - 1)
+  tokoh_quotes = json_data['result'][random_length]
+  responseQuotes = requests.get('https://backend-ihsandevs.herokuapp.com/api/quotes%20keren/?name=' + tokoh_quotes)
+  json_data_quotes = json.loads(responseQuotes.text)
+  random_length_quotes = random.randint(0,len(json_data_quotes['result']) - 1 )
+  quotes = json_data_quotes['result'][random_length_quotes]
+  return(quotes)
